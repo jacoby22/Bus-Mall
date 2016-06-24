1d0f43331fa028e5636e43aced34c04ae5ac8bac
Nick Jacoby <nickjacoby22@gmail.com>
2016-06-23 19:32:09 -0700

    created object literal and added method for getting random integer between 0 and 19

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-var images = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var imageObjects = [];
+var imageNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
-function Image(name) {
+function Img(name) {
   this.name = name;
-  images.push(this);
+  this.source = name + '.jpg';
+  imageObjects.push(this);
 }
+
+function instantiateImageObjects() {
+  for(index in imageNames) {
+    imageNames[index] = new Img(imageNames[index]);
+  }
+}
+
+instantiateImageObjects();
+
+var tracker = {
+  totalClicks: 0,
+  img1: document.getElementById('image1'),
+  img2: document.getElementById('image2'),
+  img3: document.getElementById('image3'),
+  getRandomNum: function() {
+    randomValue = Math.floor(Math.random() * imageNames.length);
+    console.log(randomValue);
+  }
+};
+
+tracker.getRandomNum();
