76ed99e4a6b5c49daf2cd114b58b264b92def47e
Nick Jacoby <nickjacoby22@gmail.com>
2016-06-23 20:13:31 -0700

    created functionality to randomly generate three unique images and place them on the page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var imageNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum'
 
 function Img(name) {
   this.name = name;
-  this.source = name + '.jpg';
+  this.source = 'assets/' + name + '.jpg';
   imageObjects.push(this);
 }
 
@@ -17,13 +17,40 @@ instantiateImageObjects();
 
 var tracker = {
   totalClicks: 0,
+  currentImages: [],
+  currentImage: null,
   img1: document.getElementById('image1'),
   img2: document.getElementById('image2'),
   img3: document.getElementById('image3'),
   getRandomNum: function() {
     randomValue = Math.floor(Math.random() * imageNames.length);
     console.log(randomValue);
+    return randomValue;
+  },
+  getImage: function() {
+    this.getRandomNum();
+    this.currentImage = imageObjects[randomValue];
+    return this.currentImage;
+  },
+  fillCurrentImages: function() {
+    var counter = 0;
+    this.currentImages.push(this.currentImage);
+    while (counter < 2) {
+      this.getImage();
+      if (this.currentImages.indexOf(this.currentImage) === -1) {
+        this.currentImages.push(this.currentImage);
+        counter += 1;
+      }
+    }
+    console.log(this.currentImages);
+  },
+  displayCurrentImages: function() {
+    this.img1.src = this.currentImages[0].source;
+    this.img2.src = this.currentImages[1].source;
+    this.img3.src = this.currentImages[2].source;
   }
 };
 
-tracker.getRandomNum();
+tracker.getImage();
+tracker.fillCurrentImages();
+tracker.displayCurrentImages();
