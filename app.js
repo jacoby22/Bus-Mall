ea2a4aeecd1e63225316305a858adf6074a63ecb
Nick Jacoby <nickjacoby22@gmail.com>
2016-06-23 19:12:34 -0700

    finished image setup and created image object constructor

---
+++ app.js
@@ -0,0 +1,6 @@
+var images = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
+function Image(name) {
+  this.name = name;
+  images.push(this);
+}
